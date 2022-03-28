7c26eaf64ebe4b8fbcf230ee1ca4a10d8c6c6903
Nickollas Nunes <nickollas_nunes@hotmail.com>
2022-03-28 08:01:43 +0200

    Commit stuff from office

--- habit_tracker_second/Program.cs
+++ habit_tracker_second/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.ReadLine();
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlTypes;
+using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit_tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    using (var tableCmd = connection.CreateCommand())
+    {
+        tableCmd.CommandText =
+            @"CREATE TABLE IF NOT EXISTS codingHours (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER
+            )";
+
+        connection.Open();
+
+        tableCmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        
+    }
+
+}
